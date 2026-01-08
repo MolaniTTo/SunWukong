d3f1982e4667b454f00b96d2b6a5e8f606c5a65f
MolaniTTo <arnaumolanorimbaua@gmail.com>
2026-01-08 16:25:40 +0100

    parry + guardado de partidas mejorado

--- Assets/Scripts/FirstSequence.cs
+++ Assets/Scripts/FirstSequence.cs
@@ -15,6 +15,7 @@ public class FirstSequence : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f); //esperem que faci el fade out
         player.animator.SetTrigger("WakeUp"); 
+        Debug.Log("First Sequence started");
         player.EnterDialogueMode(); //posem el jugador en mode diàleg (bloqueja moviments i ataca)
         screenFader.FadeIn();
         AudioManager.Instance.PlayMusic("StartSequence", 1f); //posem musica de la sequencia d'inici
