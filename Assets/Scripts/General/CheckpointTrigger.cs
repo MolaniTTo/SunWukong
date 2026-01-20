30ae5d3c45710762eeb5587206287a1a9df391c7
MolaniTTo <arnaumolanorimbaua@gmail.com>
2026-01-20 22:26:21 +0100

    luces en cada checkpoint que se guardan y se encienden al llegar al checkpoint

--- Assets/Scripts/General/CheckpointTrigger.cs
+++ Assets/Scripts/General/CheckpointTrigger.cs
@@ -1,9 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using System.Collections;
 
 public class CheckpointTrigger : MonoBehaviour
 {
 
     private bool isActivated = false;
+    public Light2D checkpointLight;
+    [SerializeField] private float activatedIntensity = 0.5f;
+    [SerializeField] private float fadeInDuration = 0.5f;
+
+    private void Start()
+    {
+        if (ProgressManager.Instance != null)
+        {
+            string checkpointID = gameObject.name;
+
+            if (ProgressManager.Instance.GetCurrentProgress().unlockedCheckpoints.Contains(checkpointID))
+            {
+                isActivated = true;
+                if (checkpointLight != null)
+                {
+                    checkpointLight.intensity = activatedIntensity;
+                }
+                Debug.Log($"Checkpoint {gameObject.name} restaurado con luz encendida");
+
+            }
+
+            else
+            {
+                if(checkpointLight != null)
+                {
+                    checkpointLight.intensity = 0f;
+                }
+            }
+
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -18,6 +51,11 @@ public class CheckpointTrigger : MonoBehaviour
 
         isActivated = true;
 
+        if (checkpointLight != null)
+        {
+            StartCoroutine(FadeInLight());
+        }
+
         // Actualizar el lastCheckPoint del jugador
         player.lastCheckPoint = transform;
 
@@ -27,9 +65,21 @@ public class CheckpointTrigger : MonoBehaviour
             ProgressManager.Instance.RegisterCheckpoint(transform);
         }
 
-        Debug.Log($"🚩 Checkpoint {gameObject.name} activado!");
+        Debug.Log($"Checkpoint {gameObject.name} activado!");
+    }
 
-        // Opcional: efecto de sonido/partículas
-        // PlayActivationEffect();
+    private IEnumerator FadeInLight()
+    {
+        float elapsed = 0f;
+        float startIntensity = checkpointLight.intensity;
+
+        while (elapsed < fadeInDuration)
+        {
+            elapsed += Time.deltaTime;
+            checkpointLight.intensity = Mathf.Lerp(startIntensity, activatedIntensity, elapsed / fadeInDuration);
+            yield return null;
+        }
+
+        checkpointLight.intensity = activatedIntensity;
     }
 }
